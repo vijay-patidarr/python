65cb4eaa3b3f38b6465978a67a9562bc308cf34b
Vijay Patidar <Vijay0patidar1@gmail.com>
2025-09-18 16:13:03 +0530

    Started Learning Python

---
+++ 01_variables.py
@@ -0,0 +1,29 @@
+#simple print statement
+print("hello world")
+
+# Variable declarations with different data types
+name = "vijay"        # string
+age = 21              # integer
+price = 9.99          # float
+
+#string concatenation and printing variables
+print("my name is " + name) 
+print("my age is ",age)
+
+# checking data types of the variables
+print(type(name))
+print(type(age))
+print(type(price))
+
+# different way to declare strings 
+name1 = 'vijay'
+name2 = "vijay"
+name3 = '''vijay'''
+name4 = """vijay"""
+print(name1,name2,name3,name4)
+
+# boolean and None Type variables
+old = False
+a = None
+print(type(old))
+print(type(a))
