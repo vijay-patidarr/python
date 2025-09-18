24fd6152ca68ed7fe4307f0e4057c65ddfb412bc
Vijay Patidar <Vijay0patidar1@gmail.com>
2025-09-18 16:16:20 +0530

    learned about operaters in pyhton

---
+++ 02_arithmeticOperation.py
@@ -0,0 +1,23 @@
+# Arithmetic Operators Example
+
+# define two numbers
+a = 4
+b = 2
+
+# addition
+print("a + b =", a + b)  
+
+# subtraction
+print("a - b =", a - b)  
+
+# multiplication
+print("a * b =", a * b)  
+
+# division
+print("a / b =", a / b)  
+
+# modulus (remainder)
+print("a % b =", a % b)  
+
+# exponentiation (power)
+print("a ** b =", a ** b)
---
+++ 03_relationalOperators.py
@@ -0,0 +1,23 @@
+# Relational Operators Example
+
+# define two numbers
+a = 5
+b = 3
+
+# greater than
+print("a > b:", a > b)
+
+# less than
+print("a < b:", a < b)
+
+# equal to
+print("a == b:", a == b)
+
+# not equal to
+print("a != b:", a != b)
+
+# greater than or equal to
+print("a >= b:", a >= b) 
+
+# less than or equal to
+print("a <= b:", a <= b)
