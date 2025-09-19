389b644d6669bc74742984a55c4b4c1e5bf66946
Vijay Patidar <Vijay0patidar1@gmail.com>
2025-09-19 18:42:41 +0530

    learned about logical operaters

---
+++ 05_logicalOperater.py
@@ -0,0 +1,13 @@
+# logical operators
+
+a = 50
+b = 30
+
+# 'and' operator: returns True if both conditions are True
+print((a > 40) and (b < 40))
+
+# 'or' operator: returns True if at least one condition is True
+print((a < 40) or (b < 40))
+
+# 'not' operator: reverses the result (True becomes False, False becomes True)
+print(not (a == b))
