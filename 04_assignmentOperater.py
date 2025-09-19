44004ac1c7bc2e3d12de54585d28be6ff2fdf923
Vijay Patidar <Vijay0patidar1@gmail.com>
2025-09-19 18:41:41 +0530

    learned about assignment operaters

---
+++ 04_assignmentOperater.py
@@ -0,0 +1,29 @@
+# Assignment Operators Example
+
+# simple assignment
+a = 10
+print("a =", a)
+
+# add and assign
+a += 5
+print("a += 5:", a)
+
+# subtract and assign
+a -= 3
+print("a -= 3:", a)
+
+# multiply and assign
+a *= 2
+print("a *= 2:", a)
+
+# divide and assign
+a /= 4
+print("a /= 4:", a)
+
+# modulus and assign
+a %= 5
+print("a %= 5:", a)
+
+# exponent and assign
+a **= 3
+print("a **= 3:", a)
