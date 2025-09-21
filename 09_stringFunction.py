9abf42ba38bb66bff01da4fc975e1b4ea76d3b15
Vijay Patidar <Vijay0patidar1@gmail.com>
2025-09-21 17:17:50 +0530

    learn about strings

---
+++ 09_stringFunction.py
@@ -0,0 +1,14 @@
+#about pprint function
+
+str = "this is a string."
+
+print(str.endswith("ng.")) # checks if the string ends with 'ng.'
+
+print(str.capitalize()) # capitalizes the first letter of the string
+
+print(str.replace("string" , "word")) # replaces 'string' with 'word'
+
+print(str.find("i")) # finds the first occurrence of 'is' in the string
+print(str.find("Q")) # returns -1 if 'Q' is not found
+
+print(str.count("i")) # counts the number of occurrences of 'i' in the string
