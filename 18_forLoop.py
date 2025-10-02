32aedc52411f717ebfaccbd7129d1d20752682f7
vijay0patidar1@gmail.com <Vijay0patidar1@gmail.com>
2025-10-02 15:03:00 +0530

    learning new concepts

---
+++ 18_forLoop.py
@@ -0,0 +1,17 @@
+list = [1,2,3,4,5,6,7,8,9,100]
+
+for el in list :
+    print(el)
+else :
+    print("end")
+
+str = "vijay"
+
+for abc in str :
+    if (abc == "j" ):
+        print("j founded")
+        break
+    else :
+         print(abc)
+else :
+ print("end")
