5cf78f133efad8ab3756a106caa235a297e09b84
vijay0patidar1@gmail.com <Vijay0patidar1@gmail.com>
2025-10-03 15:23:24 +0530

    functions

---
+++ 20_functions.py
@@ -0,0 +1,17 @@
+def calc_sum (a , b): # perameter
+    sum = a + b 
+    return sum
+# a = int(input("enter  first number: "))
+# b = int(input("enter second number: "))
+
+print( calc_sum (12 , 12)) # argument and calling function
+
+def avg (a,b,c):
+    return (a+b+c)/3
+
+a = int(input("enter  first number: "))
+b = int(input("enter second number: "))
+c = int(input("enter third number: "))
+avg = avg(a,b,c)
+print("average is: ", avg)
+
