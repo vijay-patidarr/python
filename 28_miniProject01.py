ffcf483ad025e2477026e640253f3211574d52f4
vijay0patidar1@gmail.com <Vijay0patidar1@gmail.com>
2025-10-06 20:19:12 +0530

    mini project caculate room rent

---
+++ 28_miniProject01.py
@@ -0,0 +1,22 @@
+# creating a mini project to calcylate the room rent
+
+# input we need from the user 
+print(" calculate the room rent enter following details" )
+
+Rent = int(input("eneter total amount of rent : "))
+Food  = int(input("enter total amount spendse on food :") )
+Electricity_units = int(input("enter total electricity used :") )
+charges_perUnit =  int(input("enter charges per units:") )
+roomate_number =  int(input("enter the number of person live in room :")
+)
+# totling the electricity bill multiplying units and price 
+Electricity_bill = Electricity_units * charges_perUnit
+
+#calculating total expenses we done in a month 
+total_expenses = Rent + Food + Electricity_bill
+
+print(f'total expenses of the month : {total_expenses}')
+
+# final calculation for per peroson rent 
+print (f' expenses per person : {total_expenses/roomate_number}')
+1
