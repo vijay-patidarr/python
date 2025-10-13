e96a32b823a9c661cb1e75d801b6d482f1b068ec
vijay0patidar1@gmail.com <Vijay0patidar1@gmail.com>
2025-10-13 13:02:26 +0530

    performing opeartions on array

---
+++ NUMPY/03_arrayOperarions.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+arr = np.array([10,20,30])
+print(arr)
+# we perform arithmetical opeppration on any array directly
+print(arr + 5)
+print(arr - 5)
+print(arr / 5)
+print(arr * 5)
+print(arr % 5)
+print(arr ** 5)
