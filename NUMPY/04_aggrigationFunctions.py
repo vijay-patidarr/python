92152cce75578cdf60ebbb7c6503e862877b0b13
vijay0patidar1@gmail.com <Vijay0patidar1@gmail.com>
2025-10-13 13:03:39 +0530

    agrigation functions

---
+++ NUMPY/04_aggrigationFunctions.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+arr = np.array([10,20,30,40,50])
+
+print(np.sum(arr)) # givee sum of all element of a array
+print(np.mean(arr)) # give mean of array array
+print(np.max(arr)) # give the max value in the array
+print(np.min(arr)) # give the minimum value present in the array
+print(np.std(arr)) # give standard deviation
+print(np.var(arr))
